aff92206512f34fe9ea887f1e8ae060e880f680b
Chaoran Wang <showcy@vt.edu>
2022-01-17 18:23:45 -0800

    Fix strsplit1 function.

--- R/strsplit1.R
+++ R/strsplit1.R
@@ -1,6 +1,7 @@
 #' Split a string
 #'
 #' @param x A character vector with, at most, one element.
+#' @param split The split used to split the string.
 #'
 #' @return A character vector.
 #' @export
@@ -8,10 +9,6 @@
 #' @examples
 #' x <- "alfa,bravo,charlie,delta"
 #' strsplit1(x, split = ",")
-#' strsplit1(x, split = ",", n = 2)
-#'
-#' y <- "192.168.0.1"
-#' strsplit1(y, split = stringr::fixed("."))
 strsplit1 <- function(x, split) {
   strsplit(x, split = split)[[1]]
 }
